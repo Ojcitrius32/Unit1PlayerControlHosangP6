f43b7ebbc4698a84b7bc51a70331fe711d809f30
Ojcitrius32 <732537@student.dusd.net>
2026-02-04 13:51:57 -0800

    fixxed buggs and starting on turning
    i dont know hwat else to put hhere

--- P6PlayerControlHosang/Assets/Scripts/FallowCar.cs
+++ P6PlayerControlHosang/Assets/Scripts/FallowCar.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class FallowCar : MonoBehaviour
 {
     public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         
@@ -12,6 +15,10 @@ public class FallowCar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
+    }
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
     }
 }
